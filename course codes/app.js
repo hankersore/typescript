11397dac05a8db5a96629fb9488c8d692b2b3efb
Dilshoda <alimovadilshoda47@gmail.com>
2024-02-11 23:48:01 +0500

    types in func and obj

--- course codes/app.js
+++ course codes/app.js
@@ -2,5 +2,43 @@
 const a = 1;
 let revenue = 1000;
 let bonus = 500;
+let b = 'abc';
+let c = false;
 let res = revenue + bonus;
 console.log(res);
+// types in func
+function getFullName(firstname, surname) {
+    return `${firstname} ${surname}`;
+}
+const getFullNameArrow = (firstname, surname) => {
+    return `${firstname} ${surname}`;
+};
+console.log(getFullName('Dilshoda', 'Alimova'));
+// objects
+function getFullName1(userEntity) {
+    return `${userEntity.firstname} ${userEntity.surname}`;
+}
+const user = {
+    firstname: 'Dilshoda',
+    surname: 'Alimova',
+    city: 'Tashkent',
+    age: 19,
+    skills: {
+        dev: true,
+        devOps: false,
+    }
+};
+console.log(getFullName1(user));
+// types in obj
+let info = {
+    "officeId": 45,
+    "isOpened": false,
+    "contacts": {
+        "phone": "+998330830565",
+        "email": "exampe@gmail.com",
+        "address": {
+            "city": "Tashkent"
+        }
+    }
+};
+console.log(info);
--- course codes/app.ts
+++ course codes/app.ts
@@ -6,4 +6,61 @@ let b: string = 'abc';
 let c: boolean = false;
 
 let res: number = revenue + bonus;
-console.log(res);
+// console.log(res);
+
+
+// types in func
+
+function getFullName(firstname: string, surname: string): string {
+    return `${firstname} ${surname}`
+}
+
+const getFullNameArrow = (firstname: string, surname: string): string => {
+    return `${firstname} ${surname}`
+}
+
+// console.log(getFullName('Dilshoda', 'Alimova'));
+
+
+// objects
+
+function getFullName1(userEntity: {firstname: string, surname: string}): string {
+    return `${userEntity.firstname} ${userEntity.surname}`
+}
+
+const user = {
+    firstname: 'Dilshoda',
+    surname: 'Alimova',
+    city: 'Tashkent',
+    age: 19,
+    skills: {
+        dev: true,
+        devOps: false,
+    }
+}
+
+// console.log(getFullName1(user));
+
+// types in obj
+
+let info: {
+    officeId: number,
+    isOpened: boolean,
+    contacts: { 
+        phone: string,
+        email: string,
+        address: {
+            city: string
+        }
+    }
+} = {
+    "officeId": 45,
+    "isOpened": false,
+    "contacts": {
+        "phone": "+998330830565",
+        "email": "exampe@gmail.com",
+        "address": {
+            "city": "Tashkent"
+        }
+    }
+}
