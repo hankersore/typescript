c53d834fbdf0cff9446a9388d24acd433bed0027
Dilshoda <alimovadilshoda47@gmail.com>
2024-02-15 23:10:27 +0500

    codewars exercises

--- practice/codewars.js
+++ practice/codewars.js
@@ -24,3 +24,45 @@ const centuryFromYear = (year) => {
     return century;
 };
 console.log(centuryFromYear(19));
+// 4. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function evenOdd(n) {
+    if (n % 2) {
+        return 'odd';
+    }
+    else {
+        return 'even';
+    }
+}
+console.log(evenOdd(5));
+const operations = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b
+};
+function basicOp(operation, value1, value2) {
+    const operationFunction = operations[operation];
+    return operationFunction(value1, value2);
+}
+console.log(basicOp('/', 6, 3));
+// 6. Given an array of integers. Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// If the input is an empty array  return an empty array.
+function sum(input) {
+    let positive = 0;
+    let negative = 0;
+    if (input.length === 0) {
+        return [];
+    }
+    else {
+        for (let i = 0; i < input.length; i++) {
+            if (input[i] > 0) {
+                positive++;
+            }
+            else {
+                negative += input[i];
+            }
+        }
+        return [positive, negative];
+    }
+}
+console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
--- practice/codewars.ts
+++ practice/codewars.ts
@@ -32,3 +32,58 @@ const centuryFromYear = (year: number): number => {
 };
 
 console.log(centuryFromYear(19));
+
+// 4. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOdd (n: number) : string {
+    if (n%2) {
+        return 'odd'
+    }
+    else {
+        return 'even'
+    }
+}
+
+console.log(evenOdd(5));
+
+// 5. The function should take three arguments - operation(string), value1(number), value2(number).The function should return result of numbers after applying the chosen operation.
+
+type Operation = '+' | '-' | '*' | '/';
+
+const operations: { [key in Operation]: (a: number, b: number) => number | string } = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b 
+};
+
+function basicOp(operation: string, value1: number, value2: number): number {
+    const operationFunction = operations[operation];
+    return operationFunction(value1, value2);
+}
+
+console.log(basicOp('/', 6, 3));
+
+// 6. Given an array of integers. Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// If the input is an empty array  return an empty array.
+
+function sum(input: number[]) :number[] {
+    let positive: number = 0;
+    let negative: number = 0;
+    if(input.length === 0) {
+        return [];
+    }
+    else {
+        for (let i = 0; i < input.length; i++) {
+            if (input[i]>0) {
+                positive++;
+            }
+            else {
+                negative+=input[i];
+            }
+        }
+        return [positive, negative]
+    }
+}
+
+console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
