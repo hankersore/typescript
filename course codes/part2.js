78a553e32132cde0cbe67ebc6cceb2fc3343da6b
Dilshoda <alimovadilshoda47@gmail.com>
2024-02-19 23:11:45 +0500

    implements and extends

--- course codes/part2.js
+++ course codes/part2.js
@@ -1,5 +1,14 @@
 "use strict";
 // starting from video 33 till 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // GETTER AND SETTER (doesn't work with async. good for side effects) unlike methods you don't have to call it, just assign value
 class User {
     set login(l) {
@@ -14,3 +23,51 @@ const user = new User();
 user.login = 'myLogin';
 console.log(user);
 console.log(user.login);
+class Logger {
+    log(...args) {
+        console.log(...args);
+    }
+    error(...args) {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log(...args);
+        });
+    }
+}
+class User1 {
+    pay(paymentId) {
+    }
+    delete() {
+        throw new Error("Method not implemented.");
+    }
+}
+class Payment {
+    constructor(id) {
+        this.status = 'new';
+        this.id = id;
+    }
+    pay() {
+        this.status = 'paid';
+    }
+}
+class PersistedPayment extends Payment {
+    constructor() {
+        const id = Math.random();
+        super(id);
+    }
+    save() {
+        // caves in db 
+    }
+    pay(date) {
+        super.pay();
+        if (date) {
+            this.status = 'paid';
+        }
+    }
+}
+new PersistedPayment();
+class HttpError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.code = code !== null && code !== void 0 ? code : 500;
+    }
+}
--- course codes/part2.ts
+++ course codes/part2.ts
@@ -21,3 +21,85 @@ const user = new User();
 user.login = 'myLogin';
 console.log(user);
 console.log(user.login);
+
+// IMPLEMENTS
+
+interface ILogger {
+    log(...args) :void;
+    error(...args) :void;
+}
+
+class Logger implements ILogger {
+    log(...args: any[]): void {
+        console.log(...args);
+    }
+  async error(...args: any[]): Promise<void>{
+        console.log(...args);
+    }  
+}
+interface IPayable {
+    pay(paymentId: number): void;
+    price?: number;
+}
+interface IDeletable {
+    delete(): void;
+}
+class User1 implements IPayable, IDeletable {
+    pay(paymentId: number | string): void {
+        
+    }
+    
+    delete(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+
+// EXTENDS is dependency of one class from another one with all its methods and props
+// is needed when one thing has many variants
+
+type PaymentStatus = 'new' | 'paid';
+class Payment {
+    id: number;
+    status: PaymentStatus = 'new';
+
+    constructor(id: number) {
+        this.id = id;
+    }
+
+    pay () {
+        this.status = 'paid';
+    }
+}
+
+class PersistedPayment extends Payment {
+    databaseId: number;
+    paidAt: Date;
+
+    constructor() {
+        const id = Math.random();
+        super(id);
+    }
+
+    save() {
+        // caves in db 
+    }
+
+   override pay(date?: Date) {
+        super.pay();
+        if (date) {
+            this.status = 'paid';
+        }
+    }
+}
+
+new PersistedPayment();
+
+class HttpError extends Error {
+    code: number;
+
+    constructor(message: string, code?: number) {
+        super(message);
+        this.code = code ?? 500;
+    }
+}
